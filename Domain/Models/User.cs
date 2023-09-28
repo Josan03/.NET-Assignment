8cb9dc4b95ace1fee60dd64dc2f7a855cbe027cf
Josan03 <josan.c2003@gmail.com>
2023-09-28 15:33:09 +0200

    changed folder name from Forum to WebAPI

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -6,8 +6,6 @@ public class User
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    
-    public User(){}
 
     public User(string username, string password, string email)
     {
